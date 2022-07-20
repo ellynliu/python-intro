f327500845768095e1cf7a9e5e4a47fdcfa6992d
Ellyn Liu <47698191+ellynliu@users.noreply.github.com>
2022-07-20 12:48:26 -0500

    Create gradient.py

---
+++ gradient.py
@@ -0,0 +1,20 @@
+# After redirecting the output of this Python code to an HTML file, 
+# a gradient is produced on a webpage
+
+# Make a black-to-red gradient by stacking <hr>s directly on top of one another
+
+print("<!DOCTYPE html>")
+print("<html>")
+
+print("<head>")
+print("<style>")
+print("  hr {margin-top:0; margin-bottom:0}")
+print("</style>")
+print("</head>")
+
+print("<body>")
+for x in range(256):
+  print(f'<hr style="color:rgb({x},0,0)">')
+print("</body>")
+
+print("</html>")
