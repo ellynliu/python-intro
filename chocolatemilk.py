1c6c78f65a7fb38f9eb98e1a55d770a9c1c3089f
Ellyn Liu <47698191+ellynliu@users.noreply.github.com>
2022-07-25 13:12:39 -0500

    Create chocolatemilk.py

---
+++ chocolatemilk.py
@@ -0,0 +1,54 @@
+import random
+'''
+Components of a class:
+Once you define a class, then you can create objects.
+
+__init__ - runs automatically when you create an object (instantiation)
+properties - values of the object
+methods - functions for the class
+'''
+# Here, we create a class for Dice, called Die
+class Die:
+    # In the init function, we create properties
+    # num_sides is the number of sides on the die
+    # face_up is the number on the top face of the die at that moment
+    def __init__(self, number_sides):
+        self.num_sides = number_sides
+        self.face_up = None
+    # roll_die is a method for the Die class
+    def roll_die(self):
+        self.face_up = random.randint(1, self.num_sides)
+
+# Here, we create die1 as an object of the Die class
+# Then we call the roll_die method and print out the value that the die landed on
+die1 = Die(6)
+die1.roll_die()
+print(die1.face_up)
+
+# Create a card class to represent cards
+# Each card has a value and a suit
+class Card:
+    def __init__(self, card_val, card_suit):
+        self.value = card_val
+        self.suit = card_suit
+    
+    # The desc function can be used to print a string representation of a card
+    # For example, the 1 of Spades will be returned as "Ace of Spades"
+    def desc(self):
+        val_name = self.value
+        if self.value == 1:
+            val_name = "Ace"
+        elif self.value == 11:
+            val_name = "Jack"
+        elif self.value == 12:
+            val_name = "Queen"
+        elif self.value == 13:
+            val_name = "King"
+        return f"{val_name} of {self.suit}"
+
+card1 = Card(9, "Diamonds")
+card2 = Card(1, "Hearts")
+card3 = Card(13, "Spades")
+print(card1.desc())
+print(card2.desc())
+print(card3.desc())
